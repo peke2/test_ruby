055d7dffcc2a04d50c8587927d9b455bc839b99c
peke2 <abem@dc4.so-net.ne.jp>
2015-08-11 22:18:10 +0900

    時間の比較確認を追加。

--- time/string_to_time.rb
+++ time/string_to_time.rb
@@ -1,5 +1,6 @@
 require 'time'
 
+#文字列からの変換と内容の取得
 start_at = "2015-07-14 19:50:00 +0900"
 end_at   = "2015-07-14 20:30:00 +0900"
 
@@ -15,3 +16,28 @@ puts "sec  =" + time_st.sec.to_s
 puts "開始秒=>"+time_st.to_i.to_s
 puts "終了秒=>"+time_ed.to_i.to_s
 puts "差分="+(time_ed - time_st).to_i.to_s+"秒"
+
+
+#大小比較
+ta = Time.parse("2015-07-14 19:50:00 +0900")
+tb = Time.parse("2015-07-14 20:30:00 +0900")
+tc = Time.parse("2015-07-13 00:10:00 +0900")
+td = Time.parse("2015-07-13 00:10:00 +0900")
+
+if ta < tb
+	puts "A < B"
+else
+	puts "A > B"
+end
+
+if ta < tc
+	puts "A < C"
+else
+	puts "A > C"
+end
+
+if tc === td
+	puts "C = D"
+else
+	puts "C != D"
+end
