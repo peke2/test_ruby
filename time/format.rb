0e23fb458a08200b5213f42473fbfdfc985e0e5d
peke2 <abem@dc4.so-net.ne.jp>
2015-08-14 21:50:26 +0900

    文字列での出力の際にローカル時間の設定を追加。

--- time/format.rb
+++ time/format.rb
@@ -6,3 +6,6 @@ time = Time.parse("2015-07-15 20:45:00 +0900")
 
 #「2015-07-15T20:45:00+09:00」の形で出力される
 puts time.iso8601
+
+puts time.localtime("+00:00").iso8601
+puts time.localtime("+12:00").iso8601
